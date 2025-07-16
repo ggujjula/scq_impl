e22eb9d8cd4dc3e2399863a26d044266f09802f4
none <none>
2025-07-15 17:18:23 -0700

    Add comments

--- ring_buffer.c
+++ ring_buffer.c
@@ -36,6 +36,17 @@ ring_buffer *ring_buffer_init(uint64_t num_entries, bool full) {
       return NULL;
    }
    ring->num_entries = num_entries;
+   /* Head and tail initial values:
+    * The ring buffer's head and tail values need to be set differently
+    * depending on whether the queue is initialized as empty or full.
+    * In either case, all the entries have a cycle value of 0.
+    * If the queue is empty, both the head and tail values are set to
+    * num_entries. This translates to a cycle of 1 and index of 0.
+    * If the queue is full, the head is set to 0 while the tail is set to
+    * num_entries. The is equivalent to head = {cycle 0, index 0},
+    * tail = {cycle 1, index 0}. So, the tail is exactly num_entries ahead
+    * of head.
+    */
    ring->head = full ? 0 : num_entries;
    ring->tail = num_entries;
    return ring;
@@ -55,19 +66,45 @@ static uint64_t cache_remap(uint64_t index, uint64_t num_entries) {
 }
 
 void ring_buffer_enqueue(ring_buffer *ring, uint32_t val) {
+   // Enqueuing a value involves updating the entry at the tail, and then
+   // moving the tail one element.
+   // We assume that the buffer is not full.
    uint64_t t, j;
    entry e, new_e;
    do {
+      //Attempt to load the current tail.
       t = ring->tail;
+      //Do some entry index remapping cache magic to reduce false sharing.
       j = cache_remap(ring_buffer_index(t, ring->num_entries), ring->num_entries);
+      //Read the entry from the index indicated.
       e = ring->buf[j];
+      /* If the cycle in the entry is the same as the cycle of the tail, then
+       * we know that the tail value is stale; the entry has been updated, but
+       * the tail has not yet been moved. This is because enqueues set the cycle
+       * of each appended entry to the cycle of the tail value it used to
+       * append.
+       */
       if (e.cycle == ring_buffer_cycle(t, ring->num_entries)) {
+         //Attempt to increment the tail on behalf of the other thread that
+         //already updated the entry.
          CAS(&ring->tail, &t, t+1);
          continue;
       }
+      /* If the cycle in the entry is not one behind the cycle of the tail, then
+       * the tail is stale (other enqueues have moved the tail around one loop,
+       * and updated the entry with a greater cycle).
+       * TODO: Why do we care about this case? Wouldn't the CAS on the entry
+       * fail anyways, so we would retry anyways?
+       * Answer: No, the CAS would not fail. The tail became stale between the
+       * load of the tail and the load of the entry. If the tail doesn't make
+       * another loop after entry is loaded, the CAS will succeed, which will
+       * move the cycle of the entry backwards.
+       */
       if (e.cycle + 1 != ring_buffer_cycle(t, ring->num_entries)) {
+         //Reload the tail value and try again.
          continue;
       }
+      //Construct the new entry value.
       new_e.cycle = ring_buffer_cycle(t, ring->num_entries);
       new_e.index = val;
    } while (!CAS(&ring->buf[j].val, &e.val, new_e.val));
@@ -75,19 +112,38 @@ void ring_buffer_enqueue(ring_buffer *ring, uint32_t val) {
 }
 
 bool ring_buffer_dequeue(ring_buffer *ring, uint32_t *val) {
+   // Dequeuing a value involves reading the entry at the head, and then
+   // moving the head one element.
    uint64_t h, j;
    entry e;
    do {
+      //Load the head value.
       h = ring->head;
+      //Do some entry index remapping cache magic to reduce false sharing.
       j = cache_remap(ring_buffer_index(h, ring->num_entries), ring->num_entries);
+      //Load the entry associated with the index of the head value.
       e = ring->buf[j];
+      /* We expect the entry to have the same cycle as the head value.
+       * If it does not, then one of two things have happened:
+       * 1. The entry cycle is exactly one less than the head cycle.
+       * In this case, the entry has not yet been filled with a value,
+       * which indicates the buffer is empty.
+       * 2. The entry cycle is some other value. Assuming sequential consistency,
+       * the entry cycle is then greater than the head cycle. This means that
+       * other threads have dequeued at least n entries between the load of head
+       * and the entry. The head value is stale, so we retry the loop.
+       */
       if (e.cycle != ring_buffer_cycle(h, ring->num_entries)) {
          if (e.cycle + 1 == ring_buffer_cycle(h, ring->num_entries)) {
             return false;
          }
          continue;
       }
+   //Once we checked the cycle matches, attempt to increment the head value.
+   //If it fails, then that means some other thread has already claimed this
+   //entry, so we retry the loop.
    } while (!CAS(&ring->head, &h, h+1));
+   //Read out the value from the entry.
    *val = e.index;
    return true;
 }
